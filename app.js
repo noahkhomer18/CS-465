a22d8e4165a347c0ff7a13a805012986644bb682
Noah Khomer <noahkhomer18@gmail.com>
2025-07-09 16:58:04 -0500

    Initial upload of Travlr project

---
+++ app.js
@@ -0,0 +1,15 @@
+﻿const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'app_server/views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const travellerRouter = require('./app_server/routes/traveller');
+app.use('/traveller', travellerRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("Server running on http://localhost:" + PORT);
+});
