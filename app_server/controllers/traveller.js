e00927a54703e6e22cfab990ddaf4c55c3d13435
Noah Khomer <noahkhomer18@gmail.com>
2025-07-10 20:00:47 -0500

    Module 3 complete: added trips.json, HBS rendering, MVC routing

--- app_server/controllers/traveller.js
+++ app_server/controllers/traveller.js
@@ -1,3 +1,17 @@
-﻿module.exports.travelPage = function(req, res) {
-  res.render('travel', { title: 'Travlr Getaways', description: 'Welcome to our adventure portal!' });
+﻿const fs = require('fs');
+const path = require('path');
+
+module.exports.travelPage = function(req, res) {
+  const dataPath = path.join(__dirname, '..', '..', 'trips.json');
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send("Error loading trips data.");
+    }
+    const trips = JSON.parse(data);
+    res.render('travel', {
+      title: 'Travlr Getaways',
+      description: 'Check out our awesome trips!',
+      trips: trips
+    });
+  });
 };
